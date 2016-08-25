e48006e69ed11f4869d3c583a6d8762695e94372
yanghongda <yanghongda2006@126.com>
2016-08-25 21:08:18 +0800

    完成第四章

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,Http404
-from .models import Question
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import Question,Choice
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
@@ -12,9 +13,20 @@ def detail(request,question_id):
     return render(request,'polls/detail.html',{'question':question})
 
 def results(request,question_id):
-     return HttpResponse("你正在查看的是问题%s的投票结果" %question_id)
-
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 def vote(request,question_id):
-     return HttpResponse("你正在为问题%s投票" %question_id)
-
+    p = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        #没有选中投票项的，重新显示投票菜单
+        return render(request,'polls/detail.html',{
+            'question':p,
+            'error_message':"有还没有选择",
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
 
